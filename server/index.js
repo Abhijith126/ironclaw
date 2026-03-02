ea6a8144de7944043d17918474a17e2d03280b97
Abhijith <akshay.abhijith@gmail.com>
2026-03-02 17:14:18 +0100

    Docker fixes

--- server/index.js
+++ server/index.js
@@ -17,12 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(
-  cors({
-    origin: ['http://localhost:5173', 'http://localhost:5175', 'http://127.0.0.1:5173', 'http://10.0.2.2'],
-    credentials: true
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 // Database connection
